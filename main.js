58fc28f747ad22925942040bc9caf3caa06275a3
Lexx2609 <2433053@mail.ru>
2023-06-26 12:32:14 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// for (let i = 0; i <= 50; i += 1) {
+// console.log(i)
+// }
+// for (let i = 50; i >= 25; i -= 1) {
+// console.log(i)
+// }
+
+// for (let i = 2500; i <= 3000; i ++) {
+//     if ( i % 2 != 0) {
+// console.log(i)
+//     }
+// }
+// let i = 0
+// while (i <= 50) {
+//     console.log(i)
+//     i++
+// }
+// let i = 50
+// while (i >= 25) {
+//     console.log(i)
+//     i--
+// }
+let i = 2500
+while (i <= 3000) {
+    console.log(i)
+    i++
+}
